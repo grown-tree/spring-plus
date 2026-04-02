8f3025ba5e6e426a5b05efb79be9e30afa14248d
“grown-tree” <tjsruddks12@naver.com>
2026-04-02 11:18:06 +0900

    update : JPQL 코드 수정

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -16,7 +16,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
     //개선
-    @Query("SELECT t FROM Todo t JOIN FETCH t.user WHERE " +
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user WHERE " +
             "(:weather IS NULL OR t.weather = :weather) AND " +//없으면null 있으면 해당조건으로 검색
             "(:startAt IS NULL OR t.modifiedAt >= :startAt) AND " +
             "(:endAt IS NULL OR t.modifiedAt <= :endAt) "+
