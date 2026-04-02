66d4e6b3e6356446b9a713d7fee4eefb53652bd3
“grown-tree” <tjsruddks12@naver.com>
2026-04-02 12:20:48 +0900

    fix :7. N+1문제 해결
    Comment 엔티티에 FetchType.LAZY를 통해 N+1문제 발생확인 후 CommentRepository에 join fetch로 n+1문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    //수정부분
+    @Query("SELECT c FROM Comment c JOIN fetch c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
